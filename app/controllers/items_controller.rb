9f3c3b77bcec8f161626a66a5ea2a52eba9596ea
haru3229 <kotaro.f28@gmail.com>
2021-06-15 16:45:54 +0900

    投稿機能（画面遷移）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,21 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @items = Item.new
+  end
+
+  def show
+    @item = Item.all
+  end
+
+  def create
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id, :price).merge(user_id: current_user.id)
+  end
 end
