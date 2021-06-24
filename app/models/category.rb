8e6d0cc8d94cf3fe77c8b0a3fc139cdff543f6c3
haru3229 <kotaro.f28@gmail.com>
2021-06-24 11:32:48 +0900

    README

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,16 +1,12 @@
 class Category < ActiveHash::Base
   self.data = [
     { id:  1, name: '--' },
-    { id:  2, name: '制服・体操服・私服' },
-    { id:  3, name: '制帽・赤白帽' },
+    { id:  2, name: '服（制服・体操服・私服）' },
+    { id:  3, name: '帽子（制帽・赤白帽など）' },
     { id:  4, name: '学習道具' },
-    { id:  5, name: 'おもちゃ・ゲーム' },
+    { id:  5, name: 'おもちゃ' },
     { id:  6, name: '本' },
-    { id:  7, name: '' },
-    { id:  8, name: '' },
-    { id:  9, name: '' },
-    { id: 10, name: '' },
-    { id: 11, name: '' }
+    { id:  7, name: 'その他' },
   ]
 
   include ActiveHash::Associations
