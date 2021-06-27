f7d823a86cf10e4fb567f59a6d71ee639f87f0cc
haru3229 <kotaro.f28@gmail.com>
2021-06-27 18:36:04 +0900

    rubodop

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :user
   has_one_attached :image
-  has_many   :comments
+  has_many :comments
 
   validates :content,     presence: true, unless: :was_attached?
   validates :name,        presence: true
@@ -16,7 +16,7 @@ class Item < ApplicationRecord
   end
 
   def self.search(search)
-    if search != ""
+    if search != ''
       Item.where('name LIKE(?)', "%#{search}%")
     else
       Item.all
