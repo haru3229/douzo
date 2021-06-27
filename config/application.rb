f58b69483425981a776ca173684d8b937ff14cb8
haru3229 <kotaro.f28@gmail.com>
2021-06-27 22:41:49 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
++ config.assets.initialize_on_precompile = false
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -17,3 +19,4 @@ module DouzoApp
     # the framework and any gems in your application.
   end
 end
+
