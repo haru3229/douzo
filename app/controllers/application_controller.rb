b28fbce2fca76fed93f7c8f4197da422b31e78b4
haru3229 <kotaro.f28@gmail.com>
2021-06-28 10:43:10 +0900

    herokuベーシック認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   def set_parents
     @parents = Category_search.where(ancestry: nil)
@@ -10,5 +11,11 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :introduction])
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :introduction])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
